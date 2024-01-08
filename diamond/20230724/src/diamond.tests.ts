6eca4607fd5657a8a7556d91ba0af60367899d8f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-08 23:14:29 +0100

    switch to vitest

--- diamond/20230724/src/diamond.tests.ts
+++ diamond/20230724/src/diamond.tests.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@jest/globals'
+import { expect, test } from 'vitest'
 import { horizontalBlueprintFor, diamond, includedCharactersFor, verticalBlueprintFor } from './diamond'
 
 test('explore api', () => {
