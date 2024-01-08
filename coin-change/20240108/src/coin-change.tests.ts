1aece09a4fd2e1966f1992bc1a0f6345c8af0101
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-08 23:52:58 +0100

    return matching coins

--- coin-change/20240108/src/coin-change.tests.ts
+++ coin-change/20240108/src/coin-change.tests.ts
@@ -1,8 +1,17 @@
 import {expect, test} from 'vitest'
 import {coinChange} from "./coin-change.js";
 
-test('hello', () => {
-  expect(coinChange()).toBe(false)
+
+const denomination = [1, 5, 10, 25, 100]
+
+test('no change means no coins', () => {
+  expect(coinChange(0, denomination)).toStrictEqual({1: 0, 5: 0, 10: 0, 25: 0, 100: 0})
 });
 
-
+test('just return if change matches a coin value', () => {
+  expect(coinChange(1, denomination)).toStrictEqual({1: 1, 5: 0, 10: 0, 25: 0, 100: 0})
+  expect(coinChange(5, denomination)).toStrictEqual({1: 0, 5: 1, 10: 0, 25: 0, 100: 0})
+  expect(coinChange(10, denomination)).toStrictEqual({1: 0, 5: 0, 10: 1, 25: 0, 100: 0})
+  expect(coinChange(25, denomination)).toStrictEqual({1: 0, 5: 0, 10: 0, 25: 1, 100: 0})
+  expect(coinChange(100, denomination)).toStrictEqual({1: 0, 5: 0, 10: 0, 25: 0, 100: 1})
+});
--- coin-change/20240108/src/coin-change.ts
+++ coin-change/20240108/src/coin-change.ts
@@ -1,3 +1,23 @@
-export function coinChange() {
-  return false
+export function coinChange(change: number, denomination: number[]) {
+  const result = {}
+  denomination.forEach(p => {
+    let coins = 0;
+    if (change === 1 && p === 1) {
+      coins = 1
+    }
+    if (change === 5 && p === 5) {
+      coins = 1
+    }
+    if (change === 10 && p === 10) {
+      coins = 1
+    }
+    if (change === 25 && p === 25) {
+      coins = 1
+    }
+    if (change === 100 && p === 100) {
+      coins = 1
+    }
+    Object.assign(result, {[p]: coins});
+  })
+  return result
 }
