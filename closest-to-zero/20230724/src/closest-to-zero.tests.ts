ddc05f2a171187f85877ca71e38e83fb10de70b8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-09 21:34:33 +0100

    closest to zero

---
+++ closest-to-zero/20230724/src/closest-to-zero.tests.ts
@@ -0,0 +1,6 @@
+import {expect, test} from 'vitest'
+import {closestToZero} from './closest-to-zero.js'
+
+test('hello', () => {
+  expect(closestToZero()).toEqual(true)
+});
---
+++ closest-to-zero/20230724/src/closest-to-zero.ts
@@ -0,0 +1,3 @@
+export function closestToZero() {
+  return false
+}
