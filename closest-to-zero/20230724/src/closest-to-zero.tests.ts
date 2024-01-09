c4c33b8d6ae032b0b5c46b8fa9e339a544d9e9ad
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-09 22:07:09 +0100

    group tests in describe

--- closest-to-zero/20230724/src/closest-to-zero.tests.ts
+++ closest-to-zero/20230724/src/closest-to-zero.tests.ts
@@ -1,18 +1,20 @@
-import {expect, test} from 'vitest'
+import {describe, expect, test} from 'vitest'
 import {closestToZero} from './closest-to-zero.js'
 
-test('empty list has no element closest to zero', () => {
-  expect(closestToZero([])).toEqual('no elements')
-});
+describe('closest to 0', () => {
+  test('empty list has no element closest to zero', () => {
+    expect(closestToZero([])).toEqual('no elements')
+  });
 
-test('return single entry', () => {
-  expect(closestToZero([0])).toEqual(0)
-});
+  test('return single entry', () => {
+    expect(closestToZero([0])).toEqual(0)
+  });
 
-test('return positive number closest to zero', () => {
-  expect(closestToZero([1, 2])).toEqual(1)
-});
+  test('return positive number closest to zero', () => {
+    expect(closestToZero([1, 2])).toEqual(1)
+  });
 
-test('return positive number in case there are two different numbers with the same distance to zero', () => {
-  expect(closestToZero([-1, 1])).toEqual(1)
+  test('return positive number in case there are two different numbers with the same distance to zero', () => {
+    expect(closestToZero([-1, 1])).toEqual(1)
+  });
 });
