19bb3866df3a1145b43351a5bd7382f0fa245f46
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-09 21:58:34 +0100

    refactor: use a distance metric

--- closest-to-zero/20230724/src/closest-to-zero.ts
+++ closest-to-zero/20230724/src/closest-to-zero.ts
@@ -2,5 +2,14 @@ export function closestToZero(numbers: number[]) {
   if (numbers.length === 0) {
     return 'no elements';
   }
-  return Math.min(...numbers)
+  const weighted = numbers.map((num) => {
+    return ({num, distance: Math.abs(num)});
+  });
+  const closest = weighted.reduce((acc, cur) => {
+    if (cur.distance < acc.distance) {
+      return cur
+    }
+    return acc;
+  }, {distance: Number.MAX_SAFE_INTEGER, num: Number.MAX_SAFE_INTEGER});
+  return closest.num
 }
