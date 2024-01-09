84f07132adfd23eae750e02690732cd6d86c1313
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-09 22:30:10 +0100

    write rules into comment for easy access

--- closest-to-zero/20230724/src/closest-to-zero.tests.ts
+++ closest-to-zero/20230724/src/closest-to-zero.tests.ts
@@ -31,4 +31,11 @@ describe('closest to zero', () => {
   test('strings that do not contain the zero characters are not close at all', () => {
     expect(distanceToZero('')).toEqual(Number.POSITIVE_INFINITY)
   });
+
+  /*
+    A word is close to “zero” if it contains the same letters.
+    If more than one word contains the same letters, choose the shortest one.
+    If more than one is the same length, choose the one with the letters in the most similar order.
+    If there is still a tie, choose the one that appeared first in the original list.
+   */
 });
